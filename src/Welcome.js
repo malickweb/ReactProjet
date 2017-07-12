954f4e97dcb27ac6abe5c1bd5fde193b24b51144
malickweb <malick.belgrine@gmail.com>
2017-07-12 17:10:21 +0200

    menu :active

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import './Welcome.css';
-
 //Logo
 import logo from './logo.svg';
 
@@ -11,6 +9,7 @@ class Welcome extends Component {
       isLoggedIn: true
     };
   }
+
 
   render() {
     function Welcome(props) {
@@ -24,6 +23,7 @@ class Welcome extends Component {
     );
 
     const isLoggedIn = this.state.isLoggedIn;
+    const author = 'Malick Belgrine'
 
     return (
       <div className="welcome">
@@ -31,8 +31,11 @@ class Welcome extends Component {
           <div className="row">
             <div className="col-xs-12">
               <div className="text-center">
-                <h1 id="test" className="App-title">Bienvenue sur une App React</h1>
+                <div className="cache-h1">
+                  <h1 id="test" className="App-title">Bienvenue sur une <span>App React</span></h1>
+                </div>
                 <img src={ logo } className="App-logo text-center" alt="logo" />
+                <h2>By { author }</h2>
               </div>
             </div>
           </div>
--- src/component/Header/Menu.js
+++ src/component/Header/Menu.js
@@ -7,16 +7,24 @@ import './Menu.css';
 import menus from '../../json/Menu-json';
 
 class Menu extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      isclicked: [true, false, false, false, false],
+      menu : menus
+    };
+  }
 
-  state = {
-    menus
-  };
+
 
   render() {
+    const clicked = this.state.isclicked;
+
       const sections = menus;
       const output = Object.keys(sections).map(function (section, index){
+        console.log(section);
       return (
-        <li key={ index }>
+        <li key={ index } className={ clicked[index] ? 'active' : '' }>
           <a href={ sections[section].anchorUrl }>
             { sections[section].title }
           </a>
