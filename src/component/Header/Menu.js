f789792e2a642f6536053fa9e12a9f05258f5138
malickweb <malick.belgrine@gmail.com>
2020-05-11 10:52:22 +0200

    delete comment

--- src/component/Header/Menu.js
+++ src/component/Header/Menu.js
@@ -1,4 +1,3 @@
-// import React, { Component, useState } from 'react';
 import React, { useState } from 'react';
 
 //Styles
@@ -42,7 +41,7 @@ const SubMenu = ({ handleClick }) => {
 
 const Menu = () => {
     const [isToggleOn, setIsToggleOn] = useState(false);
-    const [isCurrentPage, setIsCurrentPage] = useState('/');
+    const [isCurrentPage, setIsCurrentPage] = useState(window.location.pathname);
 
     const onClickfoo = e => {
         //e.preventDefault();
@@ -64,73 +63,3 @@ const Menu = () => {
 };
 
 export default Menu;
-
-// class SubMenu extends Component {
-//     render() {
-//         return Object.keys(menus).map((section, index) => {
-//             if (menus[section].isSousMenu === true) {
-//                 const sousMenu = menus[section].sousMenus.map((e, i) => {
-//                     return (
-//                         <li key={i}>
-//                             <a href={e.anchorUrl} onClick={this.props.handleClick}>
-//                                 {e.titre}
-//                             </a>
-//                         </li>
-//                     );
-//                 });
-//                 return (
-//                     <li key={index}>
-//                         <a href={menus[section].anchorUrl} onClick={this.props.handleClick}>
-//                             {menus[section].title}
-//                         </a>
-//                         <ul>{sousMenu}</ul>
-//                     </li>
-//                 );
-//             } else {
-//                 return (
-//                     <li key={index}>
-//                         <a href={menus[section].anchorUrl} onClick={this.props.handleClick}>
-//                             {menus[section].title}
-//                         </a>
-//                     </li>
-//                 );
-//             }
-//         });
-//     }
-// }
-
-// class Menu extends Component {
-//     state = {
-//         isToggleOn: false,
-//         isCurrentPage: '/'
-//     };
-//     onClickfoo = e => {
-//         console.log('in');
-//         this.setState({
-//             isCurrentPage: this.state.isCurrentPage
-//         });
-//         console.log('this.state.isCurrentPage', this.state.isCurrentPage);
-//         //e.preventDefault();
-//         console.log('e.currentTarget.pathname', e.target.pathname);
-//     };
-
-//     render() {
-//         return (
-//             <nav className="navbar navbar-default navbar-fixed-top">
-//                 <div className="container">
-//                     <div className="row">
-//                         <div className="col-xs-12">
-//                             <div className={this.state.isToggleOn ? 'hide' : 'show'}>
-//                                 <div className="navbar-collapse" id="bs-example-navbar-collapse-1">
-//                                     <ul className="nav navbar-nav">
-//                                         <SubMenu handleClick={this.onClickfoo} />
-//                                     </ul>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </nav>
-//         );
-//     }
-// }
--- src/component/Section/Show/Show.js
+++ src/component/Section/Show/Show.js
@@ -30,36 +30,3 @@ const Show = () => {
 };
 
 export default Show;
-
-// class Show extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             isToggleOn: false
-//         };
-//         // This binding is necessary to make `this` work in the callback
-//         this.handleClick = this.handleClick.bind(this);
-//     }
-
-//     handleClick() {
-//         this.setState(prevState => ({
-//             isToggleOn: !prevState.isToggleOn
-//         }));
-//         console.log(this.state.isToggleOn);
-//     }
-
-//     render() {
-//         return (
-//             <section>
-//                 <Menu />
-//                 <div>
-//                     <button onClick={this.handleClick}>{this.state.isToggleOn ? 'ON' : 'OFF'}</button>
-//                     <p className={this.state.isToggleOn ? 'show' : 'hide'}>
-//                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut libero vitae turpis mollis sodales. Praesent vitae ultrices nunc, nec aliquam lectus. Aenean ipsum metus, iaculis non feugiat fringilla, fermentum ac nibh. Integer scelerisque, libero vitae ultrices placerat, lectus metus euismod ex, vel pharetra nulla tellus non erat. Duis scelerisque risus diam, nec aliquam nisl sodales vel. Duis vitae faucibus risus, in viverra purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer tempor lobortis nisl, at fermentum nisl maximus vitae. Pellentesque dictum vel quam vitae auctor. Integer dignissim cursus ex, non
-//                         accumsan augue vulputate at. Proin risus mi, pharetra pellentesque ullamcorper id, elementum eu tellus. Vestibulum blandit commodo nisi vel imperdiet. Pellentesque arcu arcu, commodo non ullamcorper eu, finibus quis magna.
-//                     </p>
-//                 </div>
-//             </section>
-//         );
-//     }
-// }
