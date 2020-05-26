3d6706f1c8ff127d91b221165cc99e1286dac855
malickweb <malick.belgrine@gmail.com>
2020-05-26 11:27:19 +0200

    add emotion css

--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
-// import { css } from '@emotion/core';
+import { Component } from 'react';
+//import { css } from '@emotion/core';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import styled from '@emotion/styled';
 
 //Logo
 import logo from '../logo.svg';
@@ -15,22 +18,34 @@ class Welcome extends Component {
         function Welcome(props) {
             return <li>Hello, {props.name}</li>;
         }
-
         const tabs = ['moi', 'moi1', 'moi2'];
 
         const listTab = tabs.map((tab, index) => <Welcome key={index} name={tab} />);
 
         const isLoggedIn = this.state.isLoggedIn;
 
+        const Button = styled.button`
+            background-color: #000;
+            color: #fff;
+            border-radius: 5px;
+        `;
+
         return (
             <div>
-                <div className="container-fluid">
-                    <div className="row">
+                <div>
+                    <div>
                         <div className="col-xs-12">
                             <div className="text-center">
-                                <h1 id="test" className="App-title">
+                                <h1
+                                    css={css`
+                                        background-color: hotpink;
+                                        text-align: center;
+                                    `}
+                                    id="test"
+                                >
                                     Bienvenue sur une App React
                                 </h1>
+                                <Button> CLICK </Button>
                                 <img src={logo} className="App-logo text-center" alt="logo" />
                             </div>
                         </div>
