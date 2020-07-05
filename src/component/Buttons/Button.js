2640687832631bcd41982af30e595d0e7b6e3440
malickweb <malick.belgrine@gmail.com>
2020-07-05 05:07:53 +0200

    add function to btn

--- src/component/Buttons/Button.js
+++ src/component/Buttons/Button.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const BUTTON = styled.div`
-    display: inline-block;
-    padding: 5px 15px;
-    border: 1px solid #000;
+const DIV = styled.div`
+    button {
+        display: inline-block;
+        padding: 5px 15px;
+        border: 1px solid #000;
+        background-color: transparent;
+        transition: all 0.3s linear;
+        cursor: pointer;
+        &:hover {
+            background-color: #000;
+            color: #fff;
+            transition: all 0.3s linear;
+        }
+    }
 `;
 
-const Button = () => {
-    return <BUTTON>Button</BUTTON>;
+const Button = props => {
+    // const [isHandleClick, setIsHandleon] = useState(false);
+    // const [isToggleOn, setIsToggleOn] = useState(false);
+    // const handleClick = () => {
+    //     setIsHandleon(!isHandleClick);
+    //     setIsToggleOn(!isToggleOn);
+    //     console.log('frf');
+    // };
+    console.log('props', props);
+    return (
+        <DIV>
+            {/* <button onClick={handleClick}>{isToggleOn ? 'ON' : 'OFF'}</button> */}
+            <button onClick={props.handleClick}>{props.text}</button>
+        </DIV>
+    );
 };
 
 export default Button;
--- src/component/Section/Show/Show.js
+++ src/component/Section/Show/Show.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
+import Button from '../../Buttons/Button';
 const Show = () => {
     const [isHandleClick, setIsHandleon] = useState(false);
     const [isToggleOn, setIsToggleOn] = useState(false);
@@ -23,7 +24,7 @@ const Show = () => {
     return (
         <div>
             <Section>
-                <button onClick={handleClick}>{isToggleOn ? 'ON' : 'OFF'}</button>
+                <Button handleClick={handleClick} text={isToggleOn ? 'HIDE' : 'SHOW'} />
                 <p className={isToggleOn ? 'show' : 'hide'}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut libero vitae turpis mollis sodales. Praesent vitae ultrices nunc, nec aliquam lectus. Aenean ipsum metus, iaculis non feugiat fringilla, fermentum ac nibh. Integer scelerisque, libero vitae ultrices placerat, lectus metus euismod ex, vel pharetra nulla tellus non erat. Duis scelerisque risus diam, nec aliquam nisl sodales vel. Duis vitae faucibus risus, in viverra purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer tempor lobortis nisl, at fermentum nisl maximus vitae. Pellentesque dictum vel quam vitae auctor. Integer dignissim cursus ex, non accumsan
                     augue vulputate at. Proin risus mi, pharetra pellentesque ullamcorper id, elementum eu tellus. Vestibulum blandit commodo nisi vel imperdiet. Pellentesque arcu arcu, commodo non ullamcorper eu, finibus quis magna.
