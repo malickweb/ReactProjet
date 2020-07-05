539159828df21931d3ed0976f682ec320a49f8df
malickweb <malick.belgrine@gmail.com>
2020-07-05 20:19:04 +0200

    add function to btn

--- src/component/Buttons/Button.js
+++ src/component/Buttons/Button.js
@@ -18,17 +18,8 @@ const DIV = styled.div`
 `;
 
 const Button = props => {
-    // const [isHandleClick, setIsHandleon] = useState(false);
-    // const [isToggleOn, setIsToggleOn] = useState(false);
-    // const handleClick = () => {
-    //     setIsHandleon(!isHandleClick);
-    //     setIsToggleOn(!isToggleOn);
-    //     console.log('frf');
-    // };
-    console.log('props', props);
     return (
         <DIV>
-            {/* <button onClick={handleClick}>{isToggleOn ? 'ON' : 'OFF'}</button> */}
             <button onClick={props.handleClick}>{props.text}</button>
         </DIV>
     );
